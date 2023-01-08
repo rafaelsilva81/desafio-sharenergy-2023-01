03713c88aa01abde340b57cbefd641c83b4adf3c
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-01-08 14:05:06 -0300

    chore(web): add .gitignore and .env.example

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,11 +1,11 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 
 const api = axios.create({
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
   },
-  baseURL: "http://localhost:3333",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export { api };
