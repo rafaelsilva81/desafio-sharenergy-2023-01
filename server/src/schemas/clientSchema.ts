b0b6d342c1afcf7ab411910e9c3416302bc11af3
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-01-10 18:06:56 -0300

    feat(server): documentation with @fastify/swagger

---
+++ server/src/schemas/clientSchema.ts
@@ -0,0 +1,233 @@
+import { unauthorizedSchema } from "./errors/unauthorizedSchema";
+import { serverErrorSchema } from "./errors/serverErrorSchema";
+
+/* GET */
+const clientGetSchema = {
+  schema: {
+    tags: ["CLIENTES"],
+    description: "Obtem a lista de clientes",
+    params: {
+      type: "object",
+      properties: {
+        userId: {
+          type: "string",
+          description: "ID do usuário relacionado com esse cliente",
+        },
+        page: { type: "number" },
+      },
+      required: ["userId"],
+    },
+    response: {
+      200: {
+        description: "Cliente deletado com sucesso",
+        type: "object",
+        properties: {
+          message: { type: "string" },
+          clients: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                _id: { type: "string" },
+                name: { type: "string" },
+                email: { type: "string" },
+                phone: { type: "string" },
+                address: { type: "string" },
+                cpf: { type: "string" },
+                createdAt: { type: "string", format: "date-time" },
+                updatedAt: { type: "string", format: "date-time" },
+                user: {
+                  type: "object",
+                  properties: {
+                    username: { type: "string" },
+                  },
+                },
+                userId: { type: "string" },
+              },
+            },
+          },
+        },
+      },
+      500: { ...serverErrorSchema },
+      401: { ...unauthorizedSchema },
+    },
+    security: [
+      {
+        Bearer: [],
+      },
+    ],
+  },
+};
+
+/* POST */
+const clientPostSchema = {
+  schema: {
+    tags: ["CLIENTES"],
+    description: "Cria um novo cliente",
+    body: {
+      client: {
+        type: "object",
+        properties: {
+          _id: { type: "string" },
+          name: { type: "string" },
+          email: { type: "string" },
+          phone: { type: "string" },
+          address: { type: "string" },
+          cpf: { type: "string" },
+          userId: { type: "string" },
+        },
+      },
+    },
+    response: {
+      200: {
+        description: "Cliente deletado com sucesso",
+        type: "object",
+        properties: {
+          message: { type: "string" },
+          client: {
+            type: "object",
+            properties: {
+              _id: { type: "string" },
+              name: { type: "string" },
+              email: { type: "string" },
+              phone: { type: "string" },
+              address: { type: "string" },
+              cpf: { type: "string" },
+              createdAt: { type: "string", format: "date-time" },
+              updatedAt: { type: "string", format: "date-time" },
+              user: {
+                type: "object",
+                properties: {
+                  username: { type: "string" },
+                },
+              },
+              userId: { type: "string" },
+            },
+          },
+        },
+      },
+      400: {
+        description: "Já existe um cliente com esse email ou cpf",
+        type: "object",
+        properties: {
+          message: { type: "string" },
+        },
+      },
+      500: { ...serverErrorSchema },
+      401: { ...unauthorizedSchema },
+    },
+    security: [
+      {
+        Bearer: [],
+      },
+    ],
+  },
+};
+
+/* PUT */
+const clientPutSchema = {
+  schema: {
+    tags: ["CLIENTES"],
+    description: "Atualiza um cliente",
+    params: {
+      id: { type: "string", description: "id de um cliente" },
+    },
+    body: {
+      type: "object",
+      properties: {
+        _id: { type: "string" },
+        name: { type: "string" },
+        email: { type: "string" },
+        phone: { type: "string" },
+        address: { type: "string" },
+        cpf: { type: "string" },
+      },
+      required: [],
+      additionalProperties: true,
+    },
+    response: {
+      200: {
+        description: "Cliente atualizado com sucesso",
+        type: "object",
+        properties: {
+          message: { type: "string" },
+          client: {
+            type: "object",
+            properties: {
+              _id: { type: "string" },
+              name: { type: "string" },
+              email: { type: "string" },
+              phone: { type: "string" },
+              address: { type: "string" },
+              cpf: { type: "string" },
+              createdAt: { type: "string", format: "date-time" },
+              updatedAt: { type: "string", format: "date-time" },
+              user: {
+                type: "object",
+                properties: {
+                  username: { type: "string" },
+                },
+              },
+              userId: { type: "string" },
+            },
+          },
+        },
+      },
+      400: {
+        description: "Não existe um cliente com o ID solicitado",
+        type: "object",
+        properties: {
+          message: { type: "string" },
+        },
+      },
+      500: { ...serverErrorSchema },
+      401: { ...unauthorizedSchema },
+    },
+    security: [
+      {
+        Bearer: [],
+      },
+    ],
+  },
+};
+
+/* DELETE */
+const clientDeleteSchema = {
+  schema: {
+    tags: ["CLIENTES"],
+    description: "Deleta um cliente",
+    params: {
+      id: { type: "string", description: "id de um cliente" },
+    },
+    response: {
+      200: {
+        description: "Cliente deletado com sucesso",
+        type: "object",
+        properties: {
+          message: { type: "string" },
+        },
+      },
+      400: {
+        description: "Não existe um cliente com o ID solicitado",
+        type: "object",
+        properties: {
+          message: { type: "string" },
+        },
+      },
+      500: { ...serverErrorSchema },
+      401: { ...unauthorizedSchema },
+    },
+    security: [
+      {
+        Bearer: [],
+      },
+    ],
+  },
+};
+
+export {
+  clientGetSchema,
+  clientPostSchema,
+  clientDeleteSchema,
+  clientPutSchema,
+};
